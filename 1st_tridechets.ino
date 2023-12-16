89960d1cc7edbd5f195ea2d93677ed76e787104c
Imane Benammou <imanebenamou@gmail.com>
2023-12-16 20:45:16 +0100

    Add files via upload

---
+++ 1st_tridechets.ino
@@ -0,0 +1,198 @@
+#include <ESP8266WiFi.h>
+#include <Arduino.h>
+#include <TensorFlowLite.h>
+#include <Servo.h>
+
+// WiFi credentials
+const char* ssid = "TRI_DECHETS";
+const char* password = "IMANE123";
+
+// Create a web server on port 80
+WiFiServer server(80);
+
+// GPIO pin for LED
+const int led = LED_BUILTIN;
+
+// PIR sensor pin on GPIO D2
+const int pirSensorPin = D2;
+
+// Servo motors for trash boxes
+Servo servoBox1;
+Servo servoBox2;
+
+// Variable to track motion detection
+bool motionDetected = false;
+
+// TensorFlow Lite Interpreter
+tflite::MicroInterpreter* interpreter;
+tflite::ErrorReporter* error_reporter;
+tflite::Tensor* input;
+tflite::Tensor* output;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Configure GPIO pins
+  pinMode(led, OUTPUT);
+  pinMode(pirSensorPin, INPUT);
+
+  // Attach servo motors to respective pins
+  servoBox1.attach(/* Pin for servo controlling box 1 */);
+  servoBox2.attach(/* Pin for servo controlling box 2 */);
+
+  // Set LED pin to LOW initially
+  digitalWrite(led, LOW);
+
+  // Connect to WiFi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // Wait for WiFi connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start the web server
+  server.begin();
+
+  // Initialize TensorFlow Lite
+  error_reporter = tflite::GetErrorReporter();
+  interpreter = tflite::GetInterpreter(my_model);  // Include the generated model header file
+  interpreter->AllocateTensors();
+  input = interpreter->input(0);
+  output = interpreter->output(0);
+}
+
+void loop() {
+  // Check for motion
+  checkMotion();
+
+  // Listen for incoming clients
+  WiFiClient client = server.available();
+
+  if (!client) {
+    return;
+  }
+
+  Serial.println("New client connected.");
+
+  // Wait until the client sends some data
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the HTTP request
+  String clientResponse = client.readStringUntil('\r');
+  Serial.println(clientResponse);
+  client.flush();
+
+  // Placeholder for TensorFlow image classification
+  int motorControlValue = 0;
+
+  // If motion is detected, perform TensorFlow inference
+  if (motionDetected) {
+    // Capture and preprocess image
+    uint8_t imageData[/*size*/];  // Placeholder, replace with the actual size
+    captureImage(imageData);
+    preprocessImage(imageData);
+
+    // Run inference
+    runInference(imageData);
+
+    // Get predicted class
+    int predictedClass = getPredictedClass();
+
+    // Control ESP8266 behavior based on predictions
+    controlESP(predictedClass);
+
+    // Set motorControlValue based on predictions
+    motorControlValue = predictedClass;
+  }
+
+  // Debug
+  Serial.println("......Received Command.....");
+  Serial.println("Motor Value: ");
+  Serial.println(motorControlValue);
+  Serial.println("...........................");
+
+  // Send a response to the client
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");
+  client.print("Motor Control Value: ");
+  client.print(motorControlValue);
+}
+
+// Function to check for motion using the PIR sensor
+void checkMotion() {
+  if (digitalRead(pirSensorPin) == HIGH) {
+    motionDetected = true;
+    Serial.println("Motion Detected!");
+  } else {
+    motionDetected = false;
+  }
+}
+
+// Function to capture an image from the camera module (replace with actual implementation)
+void captureImage(uint8_t* imageData) {
+  // Your code to capture an image from the camera module
+  // ...
+}
+
+// Function to preprocess the captured image (replace with actual implementation)
+void preprocessImage(uint8_t* imageData) {
+  // Your code to preprocess the captured image
+  // ...
+}
+
+// Function to run TensorFlow Lite inference
+void runInference(uint8_t* imageData) {
+  // Set input tensor data
+  for (int i = 0; i < input->dims->size; i++) {
+    input->data.int8[i] = imageData[i];
+  }
+
+  // Run inference
+  interpreter->Invoke();
+}
+
+// Function to get the predicted class
+int getPredictedClass() {
+  // Implement logic to determine the predicted class based on model output
+  // ...
+
+  // Placeholder code (replace with actual logic)
+  return output->data.f[0] > output->data.f[1] ? 0 : 1;
+}
+
+// Function to control ESP8266 behavior based on predicted class
+void controlESP(int predictedClass) {
+  // Implement logic to control ESP8266 based on predicted class
+  // ...
+
+  // Placeholder code (replace with actual logic)pir
+  if (predictedClass == 0) {
+    Serial.println("Class 0 detected!");
+    // Open box 1
+    openBox(servoBox1);
+  } else {
+    Serial.println("Class 1 detected!");
+    // Open box 2
+    openBox(servoBox2);
+  }
+}
+
+// Function to control servo motor for opening a box
+void openBox(Servo& servo) {
+  // Set servo position to open the box
+  servo.write(/* Angle for open position */);
+  delay(1000);  // Adjust delay as needed
+  servo.write(/* Angle for closed position */);  // Close the box after delay
+}
