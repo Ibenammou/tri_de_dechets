ef14f6cdf27036a60ea1a04df9f8dfebaa58e1a4
Ibenammou <imanebenamou@gmail.com>
2024-01-21 10:41:35 +0100

    changes

--- my_correction.py
+++ my_correction.py
@@ -127,6 +127,10 @@ def create_inception_graph():
             tf.import_graph_def(graph_def, name='', return_elements=[
                 'pool_3/_reshape:0', 'DecodeJpeg/contents:0', 'ResizeBilinear:0']))
 
+        # Modify the input tensor to use placeholder_with_default
+        jpeg_data_tensor = tf.compat.v1.placeholder_with_default(
+            jpeg_data_tensor, shape=[], name='input/JPEGImage')
+
     return sess.graph, bottleneck_tensor, jpeg_data_tensor, resized_input_tensor
 
 
@@ -388,48 +392,48 @@ def variable_summaries(var):
     tf.summary.scalar('min', tf.reduce_min(var))
     tf.summary.histogram('histogram', var)
 
-
 def add_final_training_ops(class_count, final_tensor_name, bottleneck_tensor):
-  with tf.name_scope('input'):
-    bottleneck_input = tf.placeholder_with_default(
-        bottleneck_tensor, shape=[None, BOTTLENECK_TENSOR_SIZE],
-        name='BottleneckInputPlaceholder')
+    with tf.name_scope('input'):
+        bottleneck_input = tf.compat.v1.placeholder_with_default(
+            bottleneck_tensor, shape=[None, BOTTLENECK_TENSOR_SIZE],
+            name='BottleneckInputPlaceholder')
 
-    ground_truth_input = tf.placeholder(tf.float32,
-                                        [None, class_count],
-                                        name='GroundTruthInput')
+        ground_truth_input = tf.compat.v1.placeholder(tf.float32,
+                                                      [None, class_count],
+                                                      name='GroundTruthInput')
 
-  # Organizing the following ops as `final_training_ops` so they're easier
-  # to see in TensorBoard
-  layer_name = 'final_training_ops'
-  with tf.name_scope(layer_name):
-    with tf.name_scope('weights'):
-          layer_weights = tf.Variable(tf.random.truncated_normal([BOTTLENECK_TENSOR_SIZE, class_count], stddev=0.001), name='final_weights')
-  variable_summaries(layer_weights)
-  with tf.name_scope('biases'):
-    layer_biases = tf.Variable(tf.zeros([class_count]), name='final_biases')
-    variable_summaries(layer_biases)
-  with tf.name_scope('Wx_plus_b'):
-    logits = tf.matmul(bottleneck_input, layer_weights) + layer_biases
-    tf.summary.histogram('pre_activations', logits)
+    # Organizing the following ops as `final_training_ops` so they're easier
+    # to see in TensorBoard
+    layer_name = 'final_training_ops'
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            layer_weights = tf.Variable(tf.random.truncated_normal([BOTTLENECK_TENSOR_SIZE, class_count], stddev=0.001),
+                                        name='final_weights')
+        variable_summaries(layer_weights)
+        with tf.name_scope('biases'):
+            layer_biases = tf.Variable(tf.zeros([class_count]), name='final_biases')
+            variable_summaries(layer_biases)
+        with tf.name_scope('Wx_plus_b'):
+            logits = tf.matmul(bottleneck_input, layer_weights) + layer_biases
+            tf.summary.histogram('pre_activations', logits)
 
+    final_tensor = tf.nn.softmax(logits, name=final_tensor_name)
+    tf.summary.histogram('activations', final_tensor)
 
-  final_tensor = tf.nn.softmax(logits, name=final_tensor_name)
-  tf.summary.histogram('activations', final_tensor)
+    with tf.name_scope('cross_entropy'):
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+            labels=ground_truth_input, logits=logits)
+        with tf.name_scope('total'):
+            cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    tf.summary.scalar('cross_entropy', cross_entropy_mean)
 
-  with tf.name_scope('cross_entropy'):
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-        labels=ground_truth_input, logits=logits)
-    with tf.name_scope('total'):
-      cross_entropy_mean = tf.reduce_mean(cross_entropy)
-  tf.summary.scalar('cross_entropy', cross_entropy_mean)
+    with tf.name_scope('train'):
+        train_step = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(
+            cross_entropy_mean)
 
-  with tf.name_scope('train'):
-    train_step = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(
-        cross_entropy_mean)
+    return (train_step, cross_entropy_mean, bottleneck_input, ground_truth_input,
+            final_tensor)
 
-  return (train_step, cross_entropy_mean, bottleneck_input, ground_truth_input,
-          final_tensor)
 
 
 def add_evaluation_step(result_tensor, ground_truth_tensor):
